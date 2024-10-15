3ac5921be3910d5feacd87ffe7949bff20639e72
Elyumusa <elyuedu4@gmail.com>
2024-10-16 00:10:02 +0300

    Lab 5 finished

--- src/view/reset-button-component.js
+++ src/view/reset-button-component.js
@@ -8,9 +8,24 @@ function createCleanUpButton(){
 }
 
 export default class CleanUpButtonComponent extends AbstractComponent{
+    #handleClick=null;
+
+  #clickHandler=(evt)=>{
+    evt.preventDefault();
+    this.#handleClick();
+  }
+
+  constructor({onClick}){
+    super();
+    console.log("Feels gooood")
+    this.#handleClick=onClick;
+    this.element.addEventListener('click', this.#clickHandler);
+  }
     get template(){
         return createCleanUpButton();
     }
+    
+   
     // get element(){
     //     if(!this.element){
     //         this.element=createElement(this.getTemplate());
